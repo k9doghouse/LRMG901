bbe58197e0c8ceced5c01e3d43846cc0fbfe63d2
k9doghouse <k9doghouse@outlook.com>
2020-10-09 13:57:25 -0500

    Build label overlay for the chart

--- LMRG901/DetailView.swift
+++ LMRG901/DetailView.swift
@@ -57,37 +57,32 @@ struct DetailView: View {
           Text("Five Day Forecast")
             .fontWeight(.light)
           Spacer()
-        } // END: VSTACK
+        }
       } // END: GROUP(1)
       // 2
       Group {
         VStack(alignment: .leading) {
           HStack {
             Text("One day after");Spacer()
-            Text("\(station.levelOne)ft")
-              .multilineTextAlignment(.trailing)
+            Text("\(station.levelOne)ft").multilineTextAlignment(.trailing)
           }
           HStack {
             Text("Two days after");Spacer()
-            Text("\(station.levelTwo)ft")
-              .multilineTextAlignment(.trailing)
+            Text("\(station.levelTwo)ft").multilineTextAlignment(.trailing)
           }
           HStack {
             Text("Three days after");Spacer()
-            Text("\(station.levelThree)ft")
-              .multilineTextAlignment(.trailing)
+            Text("\(station.levelThree)ft").multilineTextAlignment(.trailing)
           }
           HStack {
             Text("Four days after");Spacer()
-            Text("\(station.levelFour)ft")
-              .multilineTextAlignment(.trailing)
+            Text("\(station.levelFour)ft").multilineTextAlignment(.trailing)
           }
           HStack {
             Text("Five days after");Spacer()
-            Text("\(station.levelFive)ft")
-              .multilineTextAlignment(.trailing)
+            Text("\(station.levelFive)ft").multilineTextAlignment(.trailing)
           }
-        } // END: VSTACK
+        }
         .font(.custom(condRegr, size: fntMed))
         .padding(.horizontal, padMed+padLrg)
         .padding(.bottom, 4)
@@ -104,25 +99,24 @@ struct DetailView: View {
     .lineLimit(1)
     .padding()
     .navigationBarTitle("\(station.obsDateTime)", displayMode: .inline).lineLimit(1)
-  } // END: BODY
+  }
 } // END: STRUCT(detail view)
 
   struct DrawChart: View {
+    @State var screenColor = Color(bgColor)
     let station: StationViewModel
-    let greenish: String = "greenishColor"
-
     var body: some View {
       ZStack {
-        Screen()
+        Screen().fill(screenColor).frame(maxWidth: .infinity, maxHeight: .infinity)
         Box(station: station).fill(Color(greenish))
+        Label(station: station).offset(x: 0.0, y: 50).padding(.horizontal, 40)
       }
-    } // END: BODY
+    }
   } // END: STRUCT(draw chart)
 
   struct Screen: Shape {
     func path(in rect: CGRect) -> Path {
       var path = Path()
-
       path.move(to: CGPoint(x: 0, y: 50))
       path.addQuadCurve(to: CGPoint(x: 50, y: 0), control: CGPoint(x: 0, y: 0))
       path.addLine(to: CGPoint(x: 300, y: 0))
@@ -131,24 +125,20 @@ struct DetailView: View {
       path.addQuadCurve(to: CGPoint(x: 300, y: 250), control: CGPoint(x: 350, y: 250))
       path.addLine(to: CGPoint(x: 50, y: 250))
       path.addQuadCurve(to: CGPoint(x: 0, y: 200), control: CGPoint(x: 0, y: 250))
-      path.addLine(to: CGPoint(x: 0, y: 50))
-
+      path.closeSubpath()//.addLine(to: CGPoint(x: 0, y: 50))
       let scale = (rect.width/350) * (9/10)
       let xoffset = (rect.width * (1/10))/2
       let yoffset = (rect.height - rect.height*scale) * (1/10)/2
-
       return path
         .applying(CGAffineTransform(scaleX: scale, y: scale))
         .applying(CGAffineTransform(translationX: xoffset,  y: yoffset))
-    } // END: FUNC(path)
+    }
   } // END: STRUCT(screen)
 
   struct Box: Shape {
     let station: StationViewModel
     fileprivate let room = 28
-
     func path(in rect: CGRect) -> Path {
-
       let mFive  = Float(station.minusFive)?.rounded()  ?? 0
       let mFour  = Float(station.minusFour)?.rounded()  ?? 0
       let mThree = Float(station.minusThree)?.rounded() ?? 0
@@ -160,34 +150,74 @@ struct DetailView: View {
       let lThree = Float(station.levelThree)?.rounded() ?? 0
       let lFour  = Float(station.levelFour)?.rounded()  ?? 0
       let lFive  = Float(station.levelFive)?.rounded()  ?? 0
-
       let levelArray = [mFive, mFour, mThree, mTwo, mOne, lZero, lOne, lTwo, lThree, lFour, lFive]
       var path = Path()
-
       for indx in 0..<levelArray.count {
-        path.addRoundedRect(in: CGRect(x: 20 + room*indx, y: 75, width: 22, height: Int(levelArray[indx])*2),
-                    cornerSize: CGSize(width: 3, height: 3))
-      } // END: FOR(indx)
-
+        path.addRoundedRect(in: CGRect(x: 20 + room*indx,
+                                       y: 75,
+                                   width: 22,
+                                  height: Int(levelArray[indx])*2),
+                cornerSize: CGSize(width: 3, height: 3))
+      }
       let scale = (rect.width/340) * (9/10)
       let xoffset = (rect.width * (1/11))/2
       let yoffset = (rect.height - rect.height * scale) * (1/11)/2
-
       return path
         .applying(CGAffineTransform(scaleX: scale, y: scale))
         .applying(CGAffineTransform(translationX: xoffset, y: yoffset))
-    } // END: FUNC(path)
+    }
   } // END: STRUCT(box)
 
+struct Label: View {
+  let station: StationViewModel
 
+  var body: some View {
+    let levelArray: [String] = [station.minusFive, station.minusFour, station.minusThree,
+                                station.minusTwo, station.minusOne, station.levelZero,
+                                station.levelOne, station.levelTwo, station.levelThree,
+                                station.levelFour, station.levelFive]
+    VStack {
+      HStack {
+        Spacer()
+        Text("past")
+          .foregroundColor(Color(.systemGray))
+        Spacer()
+        Text(" present")
+          .foregroundColor(Color(greenish))
+        Spacer()
+        Text("future")
+          .foregroundColor(.blue)
+        Spacer()
+      }
+      .font(.caption)
+      .opacity(0.5)
+      HStack {
+        // past
+        Group {
+          Text("\(levelArray[0])")
+          Text("\(levelArray[1])")
+          Text("\(levelArray[2])")
+          Text("\(levelArray[3])")
+          Text("\(levelArray[4])")
+        }
+        .foregroundColor(Color(.systemGray))
+        // present
+        Group {
+          Text("\(levelArray[5])")
+            .fontWeight(.semibold)
+            .foregroundColor(Color(greenish))
+        }
+        // future
+        Group {
+          Text("\(levelArray[6])")
+          Text("\(levelArray[7])")
+          Text("\(levelArray[8])")
+          Text("\(levelArray[9])")
+          Text("\(levelArray[10])")
+        }.foregroundColor(.blue)
+      }
+      .font(.caption)
+    }
+  }
+} // END: STRUCT(label)
 
-
-
-//struct DetailView_Previews: PreviewProvider {
-//  let station: StationViewModel
-//
-//  static var previews: some View {
-//    DetailView(station: station).environment(\.colorScheme, .light)
-//    DetailView(station: station).environment(\.colorScheme, .dark)
-//  }
-//}
